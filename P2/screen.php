5290f8002fbd1d91585162d865ff0848a62c34f1
Benjamin Chambers <c1631288@student.southstaffs.ac.uk>
2017-11-09 11:37:40 +0000

    lesson 09'/11/2017

---
+++ P2/screen.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Screen</title>
+    </head>
+    <body>
+        <?php
+            $_POST["width"];
+            $_POST["height"];
+            $_server['HTTP_USER_AGENT'];
+        ?>
+        <h3>
+            <?php 
+                echo "Your Browser is : $browser";
+            ?>
+        </h3>
+        <h3>
+            <?php 
+                echo "You are using a screen resolution of: $width x $hight";
+            ?>
+        </h3>
+    </body>
+</html>
---
+++ P2/screenExample.php
@@ -0,0 +1,4 @@
+<?php 
+    $browser = $_server['HTTP_USER_AGENT'];
+    Echo "Your browser is: $browser" ; 
+?>
